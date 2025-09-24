832411b3f40599f806e8607b0e2a6afc052658d3
serkantkn <serkantkn5@gmail.com>
2025-09-24 09:04:07 +0300

    24.09.2025

--- app/src/main/java/com/serkantken/secuasist/models/ContactFromPhone.kt
+++ app/src/main/java/com/serkantken/secuasist/models/ContactFromPhone.kt
@@ -1,7 +1,9 @@
 package com.serkantken.secuasist.models
 
+import java.io.Serializable
+
 data class ContactFromPhone(
     val phoneContactId: String, // Rehberdeki orijinal ID
     val displayName: String,
     val phoneOptions: MutableList<PhoneOptionForContact> = mutableListOf()
-)
+) : Serializable
--- app/src/main/java/com/serkantken/secuasist/models/PhoneOptionForContact.kt
+++ app/src/main/java/com/serkantken/secuasist/models/PhoneOptionForContact.kt
@@ -1,6 +1,8 @@
 package com.serkantken.secuasist.models
 
+import java.io.Serializable
+
 data class PhoneOptionForContact(
     val number: String,
     val typeLabel: String // Örn: "Mobil", "Ev"
-)
+) : Serializable
